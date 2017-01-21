e3ceec8dfc5164123bb04a4198f452c228c9fb47
Imran <imufun1@gmail.com>
2017-01-22 01:37:15 +0600

    object

--- Bookmarks/js/app.js
+++ Bookmarks/js/app.js
@@ -1,9 +1,17 @@
-var idd=document.getElementById('myForm').addEventListener('submit', bookmark);
+var idd=document.getElementById('myForm').addEventListener('submit', savebookmark);
 
 
-function bookmark(e) {
-  var siteName = document.getElementById('sitename');
-  console.log('siteName');
+function savebookmark(e) {
+  var siteName = document.getElementById('sitename').value;
+  var siteUrl = document.getElementById('siteurl').value;
+
+  var Bookmark = {
+    name : siteName,
+    url : siteUrl
+  }
+  console.log(Bookmark);
+  
+  console.log(siteName + "\n" + siteUrl);
 
   e.preventDefault();
 }
