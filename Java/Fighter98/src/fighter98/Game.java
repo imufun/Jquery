8746763bde409b6c9f5fbad0cfa90a005cffe2c2
imran <imufun1@gmail.com>
2017-02-07 15:48:40 +0600

    Game thread add

--- Java/Fighter98/src/fighter98/Game.java
+++ Java/Fighter98/src/fighter98/Game.java
@@ -1,13 +1,64 @@
 package fighter98;
 
-public class Game {
+public class Game implements Runnable {
 
     private Display display;
     public int width, height;
+    public String title;
+
+    private Thread thread;
+    private boolean ruuning = false;
 
     public Game(String title, int width, int height) {
+        this.title = title;
         this.width = width;
         this.height = height;
+        
+    }
+
+    private void init() {
         display = new Display(width, height, title);
     }
+
+    private void tick() {
+
+    }
+
+    private void render() {
+
+    }
+
+    @Override
+    public void run() {
+
+        init();
+        while (ruuning) {
+            tick();
+            render();
+        }
+        stop();
+    }
+
+    public synchronized void start() {
+        if (ruuning) {
+            return;
+        }
+        ruuning = true;
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    public synchronized void stop() {
+        if (!ruuning) {
+            return;
+        }
+        ruuning = false;
+        try {
+            thread.join();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
