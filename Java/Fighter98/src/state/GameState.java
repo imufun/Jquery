d979ef84dc00fa61d90c51ef1124be4a28428359
Imran <imufun1@gmail.com>
2017-02-10 12:42:07 +0600

    update

--- Java/Fighter98/src/state/GameState.java
+++ Java/Fighter98/src/state/GameState.java
@@ -15,6 +15,6 @@ public class GameState extends State{
 
     @Override
     public void render(Graphics graphics) {
-                   
+     graphics.drawImage(Assets.dirt, 0, 0, null);     
     } 
 }
