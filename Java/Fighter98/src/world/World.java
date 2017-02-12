76f15eabb3d634a907705ed3da6bf0196108ce10
imran <imufun1@gmail.com>
2017-02-12 13:03:59 +0600

    load world form text file (final)

--- Java/Fighter98/src/world/World.java
+++ Java/Fighter98/src/world/World.java
@@ -1,11 +1,13 @@
 package world;
 
 import java.awt.Graphics;
+import tilegame.DirtTile;
 import tilegame.Tile;
 
 public class World {
 
     private int width, height;
+    private int spanX, spanY;
     private int[][] tiles;
 
     public World(String path) {
@@ -18,28 +20,34 @@ public class World {
     public void render(Graphics graphics) {
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
-                getTile(x, y).render(graphics, x * Tile.TILEHEIGHT, y  * Tile.TILEHEIGHT);
+                getTile(x, y).render(graphics, x * Tile.TILEHEIGHT, y * Tile.TILEHEIGHT);
             }
         }
     }
 
     public Tile getTile(int x, int y) {
         Tile t = Tile.tiles[tiles[x][y]];
-        if (t == null)  
-            return Tile.dirTile; 
-            return t;
+        if (t == null) {
+            return Tile.dirTile;
+        }
+        return t;
     }
 
     private void loadWorld(String path) {
-        width = 5;
-        height = 5;
-        tiles = new int[width][height];
+        String file = utils.Utils.loadFileAsString(path);
+        String[] token = file.split("\\s+");
+        width = utils.Utils.parseInt(token[0]);
+        height = utils.Utils.parseInt(token[1]);
+        spanX = utils.Utils.parseInt(token[2]);
+        spanY = utils.Utils.parseInt(token[3]);
 
-        for (int x = 0; x < width; x++) {
-            for (int y = 0; y < height; y++) {
-                tiles[x][y] = 0;
+        tiles = new int[width][height];
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                tiles[x][y] = utils.Utils.parseInt(token[x + y * width + 4]);
             }
         }
+
     }
 
 }
