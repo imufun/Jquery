0e5125eeef56eb7ba8d477496274beb8b823f553
Imran <imufun1@gmail.com>
2017-02-17 09:56:09 +0600

    Animation with movement

--- Java/Fighter98/src/entity/Player.java
+++ Java/Fighter98/src/entity/Player.java
@@ -1,13 +1,18 @@
 package entity;
 
 import fighter98.Game;
+import fighter98.gfx.Animation;
 import fighter98.gfx.Assets;
 import static fighter98.gfx.Assets.mainplayer;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 import tilegame.Handler;
 
 public class Player extends Creature {
+
+    //Animation
+    private Animation AnimationDown, AnimationUp, AnimationLeft, AnimationRight;
 
     public Player(Handler handler, float x, float y) {
         super(handler, x, y, Creature.DEFAULT_CREATER_WIDTH, Creature.DEFAULT_CREATER_HIGHT);
@@ -17,10 +22,21 @@ public class Player extends Creature {
         bounds.y = 32;
         bounds.width = 32;
         bounds.height = 32;
+
+        //Animation
+        AnimationDown = new Animation(500, Assets.player_down);
+        AnimationUp = new Animation(500, Assets.player_up);
+        AnimationLeft = new Animation(500, Assets.player_left);
+        AnimationRight = new Animation(500, Assets.player_right);
+
     }
 
     @Override
     public void tick() {
+        //animation
+        AnimationDown.tick();
+        //Movement
+
         getInput();
         move();
         handler.getGameCamera().centerOnEntity(this);
@@ -64,10 +80,24 @@ public class Player extends Creature {
 
     @Override
     public void render(Graphics graphics) {
-        graphics.drawImage(Assets.mainplayer, (int) (x - handler.getGameCamera().getxOffest()), (int) (y - handler.getGameCamera().getyOffset()), width, health, null);
+        graphics.drawImage(getCurrentAnimationFrame(), (int) (x - handler.getGameCamera().getxOffest()), (int) (y - handler.getGameCamera().getyOffset()), width, health, null);
+
         //this is proper movement with camera
-        graphics.setColor(Color.red);
-        graphics.fillRect((int) (x + bounds.x - handler.getGameCamera().getxOffest()), (int) (y + bounds.y - handler.getGameCamera().getyOffset()), bounds.width, bounds.height);
+//        graphics.setColor(Color.red);
+//        graphics.fillRect((int) (x + bounds.x - handler.getGameCamera().getxOffest()), (int) (y + bounds.y - handler.getGameCamera().getyOffset()), bounds.width, bounds.height);
+    }
+
+    private BufferedImage getCurrentAnimationFrame() {
+        //return AnimationDown.getCurrentFrame();
+        if (xMove < 0) {
+            return AnimationLeft.getCurrentFrame();
+        } else if (xMove > 0) {
+            return AnimationRight.getCurrentFrame();
+        } else if (yMove < 0) {
+            return AnimationUp.getCurrentFrame();
+        } else {
+            return AnimationDown.getCurrentFrame();
+        }
     }
 
 }
---
+++ Java/Fighter98/src/fighter98/gfx/Animation.java
@@ -0,0 +1,37 @@
+package fighter98.gfx;
+
+import java.awt.image.BufferedImage;
+
+public class Animation {
+
+    private int speed, index;
+    private BufferedImage[] frames;
+
+    private long timer;
+    private long lasTime;
+
+    public Animation(int speed, BufferedImage[] frames) {
+        this.speed = speed;
+        this.frames = frames;
+        index = 0;
+        timer = 0;
+        lasTime = System.currentTimeMillis();
+    }
+
+    public void tick() {
+        timer += System.currentTimeMillis() - lasTime;
+        lasTime = System.currentTimeMillis();
+        if (timer > speed) {
+            index++;
+            timer = 0;
+            if (index >= frames.length) {
+                index = 0;
+            }
+        }
+    }
+
+    public BufferedImage getCurrentFrame() {
+        return frames[index];
+    }
+
+}
