3d45e9e9d7d5c21c4a7804944b011d3c86914fa9
Imran <imufun1@gmail.com>
2017-03-11 15:40:19 +0600

    OpenGL 3D Game  2: VAOs and VBOs

--- Java/OpenWorld/src/renderEngine/Loader.java
+++ Java/OpenWorld/src/renderEngine/Loader.java
@@ -1,7 +1,65 @@
 package renderEngine;
 
+import java.nio.FloatBuffer;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.lwjgl.BufferUtils;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL15;
+import org.lwjgl.opengl.GL20;
+import org.lwjgl.opengl.GL30;
+
 public class Loader {
 
-	//public RawModel loadToVAO(float[]position){}
+	private List<Integer> vaos = new ArrayList<Integer>();
+	private List<Integer> vbos = new ArrayList<Integer>();
+
+	public RawModel loadToVAO(float[] position) {
+
+		int vaoID = createVAO();
+		storeDataInAttributeList(0, position);
+		unbindVAO();
+		return new RawModel(vaoID, position.length / 3);
+		// Here Divided 3 means, 3 cordinartor x,y,z
+
+	}
 	
+	
+	private void cleanUp(){
+		for(int vao:vaos){
+			GL30.glDeleteVertexArrays(vao);
+		}
+		for(int vbo:vbos){
+			GL15.glDeleteBuffers(vbo);
+		}
+	}
+
+	public int createVAO() {
+		int vaoID = GL30.glGenVertexArrays();
+		GL30.glBindVertexArray(vaoID);
+		return vaoID;
+	}
+
+	public void storeDataInAttributeList(int attributeNumber, float[] data) {
+		int vboID = GL15.glGenBuffers();
+		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vboID);
+		FloatBuffer buffer = storeDataBuffer(data);
+		GL15.glBufferData(GL15.GL_ARRAY_BUFFER, buffer, GL15.GL_STREAM_DRAW);
+		GL20.glVertexAttribPointer(attributeNumber, 3, GL11.GL_FLOAT, false, 0, 0);
+		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
+	}
+
+	public void unbindVAO() {
+		GL30.glBindVertexArray(0);
+	}
+
+	private FloatBuffer storeDataBuffer(float[] data) {
+		FloatBuffer buffer = BufferUtils.createFloatBuffer(data.length);
+		buffer.put(data);
+		buffer.flip();
+		return buffer;
+	}
+
 }
