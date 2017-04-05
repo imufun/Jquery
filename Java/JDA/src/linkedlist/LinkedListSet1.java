d2534d7bd484725ba9b539e74c7c8bc28fbfc522
imran <imufun1@gmail.com>
2017-04-05 16:03:57 +0600

    add new thing

---
+++ Java/JDA/src/linkedlist/LinkedListSet1.java
@@ -0,0 +1,17 @@
+package linkedlist;
+ 
+
+public class LinkedListSet1 {
+
+   // Node head;
+
+//    static class Node(){
+//        int data;
+//        Node next;
+//
+//        Node(int d) {
+//            data = d;
+//            next = null;
+//        }
+//    }
+}
