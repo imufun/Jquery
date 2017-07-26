53c45b0f29609d825f7f2419bc2657651c7c521d
Imran <imufun1@gmail.com>
2017-07-26 23:35:08 +0600

    add

--- Java/AutoBoxingTest.java
+++ Java/AutoBoxingTest.java
@@ -5,7 +5,7 @@ public class AutoBoxingTest{
 		//System.out.println("hello world");
 	//}
 	
-	public static void main(String  args[a]){
+	public static void main(String  args[]){
 		
 		//AutoBoxingTest autoBoxingTest= new AutoBoxingTest();
 		//autoBoxingTest.Hello();
