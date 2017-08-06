611d9366b0dbfcb92a71b28a80c121d621314f1f
imran <imufun1@gmail.com>
2017-08-06 17:48:55 +0600

    UVA problem

---
+++ Java/JDA/src/uva/The_3n_1_problem_100.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uva;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author imran
+ */
+public class The_3n_1_problem_100 {
+
+    public static void main(String[] args) {
+
+        int n = 0;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter number of Element");
+        n = input.nextInt();
+        int r[] = new int[n];
+
+        System.out.println("Enter any number ");
+        for (int i = 0; i < n; i++) {
+            r[i] = input.nextInt();
+
+        }
+
+//        if (n == 1) {
+//            if (n % 2 == 0) {
+//                
+//            }
+//        }
+    }
+
+}
